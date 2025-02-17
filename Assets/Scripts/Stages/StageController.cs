26e616b208ae96838f750f1bd5509d20d905f4df
Hunter Dyar <bloop@hdyar.com>
2025-02-17 16:00:53 -0500

    Stage Controller

--- Assets/Scripts/Stages/StageController.cs
+++ Assets/Scripts/Stages/StageController.cs
@@ -8,6 +8,8 @@ public class StageController : MonoBehaviour
 {
     public List<Stage> _stages;
 	[CanBeNull] private Stage _currentStage;
+	private Stack<Stage> _history = new Stack<Stage>();
+	
     void Start()
     {
         var defaultStage = _stages[0];
@@ -19,12 +21,14 @@ public class StageController : MonoBehaviour
 	    if(_currentStage != null)
 	    {
 		    StartCoroutine(SwitchToScene(newStage));
+		    _history.Push(newStage);
+		    //if this stage extends, it's a child. If we are going back, remove.
 	    }
-
 	    else
 	    {
 		    SceneManager.LoadScene(newStage.sceneName, LoadSceneMode.Additive);
 		    _currentStage = newStage;
+		    _history.Push(newStage);
 	    }
     }
 
@@ -34,5 +38,16 @@ public class StageController : MonoBehaviour
 	    yield return SceneManager.LoadSceneAsync(nextStage.sceneName, LoadSceneMode.Additive);
 	    _currentStage = nextStage;
 	}
-    
+
+	public bool GoBackInHistory()
+	{
+		if(_history.Count > 1)
+		{
+			_history.Pop();//remove current
+			var previousStage = _history.Peek();//pop... and instantly re-push it when we call LoadStage.
+			StartCoroutine(SwitchToScene(previousStage));
+			return true;
+		}
+		return false;
+	}
 }
